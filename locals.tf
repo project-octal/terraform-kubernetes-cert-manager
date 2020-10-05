3ccd3889611f0d950c46c1df3e7a001d0a2f05c3
Dylan Turnbull <dylanturn@gmail.com>
2020-10-05 12:01:25 -0500

    terraformf mt

--- locals.tf
+++ locals.tf
@@ -1,10 +1,10 @@
 resource "random_pet" "instance_name" {}
 locals {
-  name = "cert-manager"
+  name        = "cert-manager"
   instance_id = random_pet.instance_name.id
   labels = merge({
-    "app.kubernetes.io/part-of" = local.name
-    "app.kubernetes.io/instance" = local.instance_id
+    "app.kubernetes.io/part-of"    = local.name
+    "app.kubernetes.io/instance"   = local.instance_id
     "app.kubernetes.io/managed-by" = "terraform"
   }, var.labels)
 }
--- main.tf
+++ main.tf
@@ -5,29 +5,29 @@ module "custom_resource_definitions" {
 module "cert_manager_cainjector" {
   source = "./cert-manager-cainjector"
 
-  instance_id = local.instance_id
-  namespace = kubernetes_namespace.namespace.metadata.0.name
-  image_repository = var.image_repository
+  instance_id       = local.instance_id
+  namespace         = kubernetes_namespace.namespace.metadata.0.name
+  image_repository  = var.image_repository
   image_pull_policy = var.image_pull_policy
-  labels = local.labels
+  labels            = local.labels
 }
 
 module "cert_manager" {
   source = "./cert-manager"
 
-  instance_id = local.instance_id
-  namespace = kubernetes_namespace.namespace.metadata.0.name
-  image_repository = var.image_repository
+  instance_id       = local.instance_id
+  namespace         = kubernetes_namespace.namespace.metadata.0.name
+  image_repository  = var.image_repository
   image_pull_policy = var.image_pull_policy
-  labels = local.labels
+  labels            = local.labels
 }
 
 module "cert_manager_webhook" {
   source = "./cert-manager-webhook"
 
-  instance_id = local.instance_id
-  namespace = kubernetes_namespace.namespace.metadata.0.name
-  image_repository = var.image_repository
+  instance_id       = local.instance_id
+  namespace         = kubernetes_namespace.namespace.metadata.0.name
+  image_repository  = var.image_repository
   image_pull_policy = var.image_pull_policy
-  labels = local.labels
+  labels            = local.labels
 }

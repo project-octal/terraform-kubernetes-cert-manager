132e3f826b2c7f9703513a75d5ed758acf0bffc5
Dylan Turnbull <dylanturn@gmail.com>
2020-10-06 10:19:12 -0500

    the webhook deployment was missing a label

--- cert-manager-webhook/locals.tf
+++ cert-manager-webhook/locals.tf
@@ -2,6 +2,7 @@ locals {
   image_repository = var.image_repository == null ? "quay.io" : var.image_repository
   app              = "webhook"
   labels = merge({
+    "app"                         = local.app
     "app.kubernetes.io/component" = var.component
     # Some more labels go here...
   }, var.labels)

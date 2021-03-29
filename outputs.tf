712a90c438b56b7352a02353ad0d9c4335b8fea9
Dylan Turnbull <dylanturn@gmail.com>
2021-03-29 17:46:48 -0500

    make sure the default issuer is only output once it is really ready

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,12 @@
 output "cert_issuer" {
+  # The issuer is only output once it's ready to issue certs.
+  # (Prevent race condition)
+  depends_on = [
+    module.custom_resource_definitions,
+    module.cert_manager_cainjector,
+    module.cert_manager,
+    module.cert_manager_webhook,
+    module.cert_manager_issuers
+  ]
   value = module.cert_manager_issuers.default_issuer
 }

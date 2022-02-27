e3c17eb2d1fbc8c0fd0294c7b12e7a80e6385cd1
Dylan Turnbull <dylanturn@gmail.com>
2022-02-27 13:00:16 -0600

    remove the block for cert-manager-issuers. This will now be its own module

--- main.tf
+++ main.tf
@@ -5,8 +5,6 @@ module "custom_resource_definitions" {
 module "cert_manager_cainjector" {
   source = "./cert-manager-cainjector"
 
-  depends_on = [module.custom_resource_definitions]
-
   instance_id       = local.instance_id
   namespace         = kubernetes_namespace.namespace.metadata.0.name
   image_repository  = var.image_repository
@@ -16,8 +14,6 @@ module "cert_manager_cainjector" {
 
 module "cert_manager" {
   source = "./cert-manager"
-
-  depends_on = [module.custom_resource_definitions]
 
   instance_id       = local.instance_id
   namespace         = kubernetes_namespace.namespace.metadata.0.name
@@ -29,21 +25,9 @@ module "cert_manager" {
 module "cert_manager_webhook" {
   source = "./cert-manager-webhook"
 
-  depends_on = [module.custom_resource_definitions]
-
   instance_id       = local.instance_id
   namespace         = kubernetes_namespace.namespace.metadata.0.name
   image_repository  = var.image_repository
   image_pull_policy = var.image_pull_policy
   labels            = local.labels
-}
-
-module "cert_manager_issuers" {
-  source      = "./cert-manager-issuers"
-
-  depends_on = [module.custom_resource_definitions]
-
-  namespace   = kubernetes_namespace.namespace.metadata.0.name
-  letsencrypt = var.certificate_issuers.letsencrypt
-  # Others will go here...
 }

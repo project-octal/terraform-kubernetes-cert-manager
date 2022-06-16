a41b1d04c89f237bb93d71f7f7f4a0a643fb7048
Dylan Turnbull <dylanturn@gmail.com>
2022-06-16 13:32:32 -0500

    added permissions for getting leases

--- cert-manager/role.tf
+++ cert-manager/role.tf
@@ -6,15 +6,16 @@ resource "kubernetes_role" "role" {
       "app.kubernetes.io/name" = var.name
     }, local.labels)
   }
-  rule {
-    # Used for leader election by the controller
-    # TODO: refine the permission to *just* the leader election configmap
-    api_groups = [""]
-    resources = [
-      "configmaps"
-    ]
-    verbs = ["get", "create", "update", "patch"]
-  }
+  # rule {
+  #   # Used for leader election by the controller
+  #   # TODO: refine the permission to *just* the leader election configmap
+  #   api_groups = [""]
+  #   resources = [
+  #     "configmaps"
+  #   ]
+  #   verbs = ["get", "create", "update", "patch"]
+  # }
+
   rule {
     api_groups = ["coordination.k8s.io"]
     resources = [
